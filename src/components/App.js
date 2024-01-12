8fdd2355d1f693a52b38eb205c41a0c2809746ce
UtsavKumar126/ <guptautsavkumar126@gmail.com>
2024-01-12 18:14:20 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,43 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { useState } from "react";
 
+
+const App=()=>{
+    const[todo,setTodo]=useState("");
+    const[count,setCount]=useState(0);
+    const[list,setList]=useState([]);
+    const[item,setItem]=useState("");
+
+    function addToList(e){
+        if(item.length<5)return;
+        e.preventDefault();
+        setList([...list,item]);
+        setItem("");
+    }
+    return(
+        <div>
+            <h1>My todos</h1>
+            <div>{todo}</div>
+            <button id="add-todo" onClick={()=>setTodo("New todo")}>Add Todo</button>
+            <hr/>
+            <div>Count: {count}
+                <button id="increment" onClick={()=>setCount(count+1)}>+</button>
+            </div>
+            <hr/>
+            <hr/>
+            <form onSubmit={addToList}>
+                <input placeholder="Add Skill" onChange={(e)=>setItem(e.target.value)} value={item}/>
+                <button>Add Skill</button>
+            </form>
+            <ul>
+                {
+                    list.map((ele,i)=>
+                    <li key={i}>{ele}</li>
+                    )
+                }
+            </ul>
+
+        </div>
+    )
+}
+
+export default App
